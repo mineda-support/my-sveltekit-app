47183eb1403c11ecd9ff9a2c2b9c6a810a66971e
seijirom <seijiro.moriyama@gmail.com>
2023-07-25 00:02:58 +0900

    plotly use textarea

--- my-app/src/routes/plotly/+page.svelte
+++ my-app/src/routes/plotly/+page.svelte
@@ -1,17 +1,37 @@
 <script lang="ts">
     import Plot from 'svelte-plotly.js';
-  
-    const data = [
-      {
-        x: [1, 2, 3, 4, 5],
-        y: [1, 2, 4, 8, 16]
-      }
-    ];
+    // import data from './data.js'
+    let text = "1\t2\n3\t4\n5\t1";
+
+   function load_data(text) {
+     let data = {x: [], y: []};
+     let lines = text.split("\n");
+     let a, b;
+     for (let line of lines) {
+       console.log('line:'+line);
+       //[a, b] = line.replace(/ +/, ' ').split(' ').map(a => Number(a));
+       [a, b] = line.split("\t").map(a => Math.abs(Number(a)));
+       data.x.push(a);
+       data.y.push(b);
+     };
+     console.log(data);
+     return data;
+    }
+
+    $: data = load_data(text);
   </script>
-  
+  <textarea bind:value={text} />
+ 
+  <style>
+    textarea {
+      width: 100%;
+      height: 200px;
+    }
+  </style>
+
   <Plot
-    {data}
-    layout={{
+    data = {[data]}
+    layout={{ yaxis: { type: 'log', autorange: 'true'},
       margin: { t: 0 }
     }}
     fillParent='width'
