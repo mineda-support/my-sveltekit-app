cca3cc2f4977c328067e43c9064463deec63f522
seijirom <seijiro.moriyama@gmail.com>
2023-07-31 14:48:16 +0900

    lsm_plot experiment

--- my-app/src/routes/lsm_plot/+page.svelte
+++ my-app/src/routes/lsm_plot/+page.svelte
@@ -1,45 +1,52 @@
 <script lang="ts">
-    import type { lengthToDegrees } from "plotly.js-dist";
   import Plot from "svelte-plotly.js";
 
-  let text = "1\t2\n3\t4\n5\t1";
-  let files;
-  /*
-  let csv;
-	$: if (files) {
-		// Note that `files` is of type `FileList`, not an Array:
-		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
-		console.log(files);
+  let files = [];
+  let texts = [];
+  $: if (files) {
+    console.log(files);
+    for (const file of files) {
+      console.log(`${file.name}: ${file.size} bytes`);
+      (async () => {
+        //const text = await fetchText(file);
+        let text = await file.text();
+        texts.push(text);
+        texts = texts; // necessary to make texts reactive
+      })();
+    }
+  }
 
-		for (const file of files) {
-			console.log(`${file.name}: ${file.size} bytes`);
-      const reader = new FileReaderSync();
-      reader.onload = () => {
-        let txt = await file.text();
-        console.log(txt);
-      };
-		}
-	}
-  */
-
-  function load_data(text) {
+  let text_data = "1\t2\n3\t4\n5\t1";
+  let start = 0;
+  let stop = text_data.length;
+  let left; let right;
+  function load_data(text, start, stop, left, right) {
     let data = { x: [], y: [] };
     let lines = text.split("\n");
     let a, b;
-    for (let line of lines) {
+    /* for (let line of lines) { */
+    for (let i = 0; i < lines.length; i++) {
+      if (i < start || i > stop) continue;
+      let line = lines[i];
       if (line.length == 0) continue;
       // console.log("line:" + line);
       //[a, b] = line.replace(/ +/, ' ').split(' ').map(a => Number(a));
-      [a, b] = line.split(/[\t ,]/).map((a) => Math.abs(Number(a)));
+      console.log(line);
+      [a, b] = line.split(/[\t ,]/).map((a) => Math.abs(Number(a))).slice(left, right+1);
       data.x.push(a);
       data.y.push(b);
     }
     console.log(data);
     return data;
   }
-
-  $: data = load_data(text);
-
+  //$: data = load_data(text_data, start, stop);
+  let data;
+  $: if (files && files.size > 0) {
+    text_data = texts[0];
+    data = load_data(text_data, start, stop, left, right);
+  } else {
+    data = load_data(text_data, start, stop, left, right);
+  }
   //const coordinates = [];
   // 回帰直線を求める（最小二乗法）
   const lsm = (coordinates) => {
@@ -66,40 +73,56 @@
     console.log("data=", data);
     console.log("coordinates=", coordinates);
     console.log("a=", a, " b=", b);
-    is = 10**b;
-    nvt = Math.log10(Math.E)/a;
-    console.log('is=', is, ' nvt=', nvt);
+    is = 10 ** b;
+    nvt = Math.log10(Math.E) / a;
+    console.log("is=", is, " nvt=", nvt);
 
     let sim_data = { x: [], y: [] };
     for (let c of coordinates) {
       sim_data.x.push(c.x);
-      sim_data.y.push(is*Math.exp(c.x/nvt));
+      sim_data.y.push(is * Math.exp(c.x / nvt));
     }
     console.log(sim_data);
     return sim_data;
   }
-  
+
   let xmin;
   let xmax;
+  
   $: fit_data = lsm_fit(data, xmin, xmax);
-
 </script>
-<label for="csv">Upload csv files</label>
-<input bind:files id="many" type="file" accept="csv" />
-<textarea bind:value={text} />
+
+<input type="file" bind:files />
+
+{#if files}
+  <h2>Selected files:</h2>
+  {#each Array.from(files) as file, i}
+    <p>{file.name} ({file.size} bytes)</p>
+    <p>
+      e: {`texts[$i]`}
+      i: {i}
+    </p>
+  {/each}
+  <p>files length: {files.length}</p>
+{/if}
+
 <div>
-<label>xmin: <input type="number" step="0.01" bind:value={xmin} 
-  placeholder= {data.x[0]} /></label>
-<label>xmax: <input type="number" step="0.01" bind:value={xmax}
-  placeholder= {data.x[data.x.length-1]}/></label>
+  <label>start line: <input type="number" bind:value={start} /></label>
+  <label>stop line: <input type="number" bind:value={stop} /></label>
+  <label>left column: <input type="number" bind:value={left} /></label>
+  <label>right column: <input type="number" bind:value={right} /></label>
 </div>
+
+<textarea bind:value={text_data} />
+
 <label>is = {is} nvt = {nvt}</label>
-  <Plot
+<Plot
   data={[data, fit_data]}
-  layout={{ yaxis: {type: 'log'}, margin: { t: 0 } }}
+  layout={{ yaxis: { type: "log" }, margin: { t: 0 } }}
   fillParent="width"
   debounce={250}
 />
+
 <style>
   textarea {
     width: 100%;
