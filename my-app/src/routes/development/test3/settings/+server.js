26a35dfb35ac94defb9b49d74391701b16c163bb
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2024-01-17 19:55:20 +0900

    load settings WIP

--- my-app/src/routes/development/test3/settings/+server.js
+++ my-app/src/routes/development/test3/settings/+server.js
@@ -3,24 +3,22 @@ import glob from 'glob';
 import path from 'path';
 import { json } from '@sveltejs/kit';
 
-export function GET() {
-    //const dir = process.env.HOME + '/Seafile/PTS06_2022_8/BGR_TEG/';
-	const home = process.env.HOME.replaceAll('\\', '/');
-    const json_file = home + '/Seafile/PTS06_2022_8/BGR_TEG/tsd_TEG_tb.json';
+export function GET({ url }) {
+    const settings_name = url.searchParams.get('settings_name');
+    const dir = url.searchParams.get('dir');
+    const json_file = dir + settings_name + '_settings.json';
     console.log(json_file);
-    console.log('comparison: ' + (json_file == 'C:/Users/seijirom/Seafile/PTS06_2022_8/BGR_TEG/tsd_TEG_tb.json'));
-    const [vars, plotdata] = JSON.parse(fs.readFileSync(json_file));
-    console.log(plotdata);
-    return json(plotdata);
+    const settings = JSON.parse(fs.readFileSync(json_file));
+    return json(settings.equation);
 }
 
 export async function POST({ request, cookies }) {
     // console.log(request);
 	const props = await request.json();
     const wdir = props.wdir;
-    const settings_file = props.settings_file;
+    const settings_name = props.settings_name;
     console.log(wdir);
-    fs.writeFileSync(wdir+`${settings_file}_settings.json`, JSON.stringify(props));
+    fs.writeFileSync(wdir+`${settings_name}_settings.json`, JSON.stringify(props));
     console.log(props);
     const setting_files = glob.sync(wdir + '*_settings.json');
 	return json(setting_files.map(a => path.basename(a).replace('_settings.json', '')), { status: 201 });
